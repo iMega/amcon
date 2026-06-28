d2adae6431d381c16534ba128a049167d486695d
Dmitry Stoletov <irvis@imega.ru>
2026-06-28 11:42:18 +0300

    add contact form
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { siteUrl } from "./site.config.js";
 
 export default defineConfig({
+    site: siteUrl,
     output: "static",
 });
